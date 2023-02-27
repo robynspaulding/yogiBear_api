92baadadb269d47569a983dcf4e9507b968bcb6c
RobynSpaulding <robynm27@gmail.com>
2023-02-27 14:23:32 -0800

    spacing in models

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,5 @@
 class Booking < ApplicationRecord
   belongs_to :yogi
+  
   validates :date, :start_time, :end_time, :total_price, :address, :city, :state, :event_type, :in_person, :paid, :email, presence: true
 end
--- app/models/yogi.rb
+++ app/models/yogi.rb
@@ -1,4 +1,5 @@
 class Yogi < ApplicationRecord
   has_many :bookings
+  
   validates :name, :rate, :yoga_type, :bio, :contact, :city, :state, :available_start_time, :available_end_time presence: true
 end
