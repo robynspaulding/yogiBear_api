7422f686a476e2899149e32e3919cbfafb3fa1e7
RobynSpaulding <robynm27@gmail.com>
2023-02-27 14:10:47 -0800

    adds validations to booking and yogi models, adds email column to bookings table

---
+++ db/migrate/20230227220937_add_email_to_booking.rb
@@ -0,0 +1,5 @@
+class AddEmailToBooking < ActiveRecord::Migration[7.0]
+  def change
+    add_column :bookings, :email, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_27_214953) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_27_220937) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -29,6 +29,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_27_214953) do
     t.boolean "paid"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
     t.index ["yogi_id"], name: "index_bookings_on_yogi_id"
   end
 
