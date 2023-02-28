e74be4e194b4b06531401a37d75d0c578bdd5d04
RobynSpaulding <robynm27@gmail.com>
2023-02-27 20:00:53 -0800

    adds User controller and model with create action and validations

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,18 @@
+class UsersController < ApplicationController
+
+  def create
+    user = User.create!(
+      name: params[:name],
+      email: params[:email],
+      password: params[:password],
+      password_confirmation: params[:password_confirmation],
+      is_admin: params[:is_admin]
+    )
+    if user.save
+      render json: { message: "User created successfully" }, status: :created
+    else
+      render json: { errors: user.errors.full_messages }, status: :bad_request
+    end
+  end
+  
+end
